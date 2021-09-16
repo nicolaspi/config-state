bbf99c7084b88a4a491f7de3fcf27eeb619c7036
Nicolas Pinchaud <nicolas.pinchaud@gmail.com>
2021-09-16 11:07:46 +0200

    - exclude tests

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nicolaspi/config-state",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests', 'examples')),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
